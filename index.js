8e0db0c0a81ecc487e0873c0cb9c0fa41b73bdc6
kain <kainpro@foxmail.com>
2019-04-19 15:40:28 +0800

    Submit

---
+++ index.js
@@ -0,0 +1,17 @@
+const env = require('dotenv').config().parsed;
+const amqp = require('amqplib');
+
+amqp.connect(env.amqp_uri).then(async (connect) => {
+    try {
+        const channel = await connect.createChannel();
+        channel.assertExchange(env.exchange, 'topic', {durable: true});
+        channel.assertQueue(env.queue, {durable: true});
+        channel.bindQueue(env.queue, env.exchange);
+        channel.consume(env.queue, async (msg) => {
+            console.log(msg);
+        }, {noAck: false});
+    } catch (e) {
+        console.log(e)
+    }
+});
+
