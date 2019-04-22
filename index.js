f08f345675f7f76af867b577c66f53ae0b9ce3ab
kain <kainpro@foxmail.com>
2019-04-22 09:13:29 +0800

    Submit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const env = require('dotenv').config().parsed;
 const amqp = require('amqplib');
+const joi = require('joi');
+const {MongoClient} = require('mongodb');
+const client = new MongoClient(env.mongo_uri, {useNewUrlParser: true});
 
 amqp.connect(env.amqp_uri).then(async (connect) => {
     try {
@@ -8,10 +11,26 @@ amqp.connect(env.amqp_uri).then(async (connect) => {
         channel.assertQueue(env.queue, {durable: true});
         channel.bindQueue(env.queue, env.exchange);
         channel.consume(env.queue, async (msg) => {
-            console.log(msg.content.toString());
+            try {
+                const param = JSON.parse(msg.content.toString());
+                const validate = joi.validate(param, joi.object({
+                    namespace: joi.string().required(),
+                    data: joi.required()
+                }));
+                if (validate.error !== null) {
+                    channel.ack(msg);
+                }
+
+
+            } catch (e) {
+                channel.ack(msg);
+            }
         }, {noAck: false});
     } catch (e) {
         console.log(e)
     }
 });
 
+client.connect((err) => {
+    console.log(err);
+});
