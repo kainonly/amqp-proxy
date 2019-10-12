51a8f9b3262d52d46a7671735318bbdcd60dc4a4
kain <zhangtqx@vip.qq.com>
2019-10-12 15:12:05 +0800

    Submit

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,11 +1,23 @@
-import { env } from 'process';
+import { readFileSync, existsSync } from 'fs';
+import { join } from 'path';
 import { subscribe } from './subscribe';
 import { AmqpService } from './common/amqp.service';
 import { ElasticService } from './common/elastic.service';
 
-AmqpService.registered(env).ready().then(channel => {
-  const elastic = new ElasticService();
+const configPath = join(__dirname, 'config.json');
+if (!existsSync(configPath)) {
+  console.log('must set config.json');
+  process.exit(1);
+}
+const config: any = JSON.parse(readFileSync(configPath, {
+  encoding: 'utf8',
+}));
 
+AmqpService.registered(config.amqp).then(channel => {
+  subscribe(
+    channel,
+    ElasticService.create(config.elastic).getClient(),
+  );
 }).catch(err => {
   if (err) {
     console.log(err);
--- src/common/amqp.service.ts
+++ src/common/amqp.service.ts
@@ -1,37 +1,17 @@
-import { connect } from 'amqplib/callback_api';
+import { Channel, connect } from 'amqplib/callback_api';
 
 export class AmqpService {
   private options = {};
 
   static registered(options: any) {
-    return new AmqpService(options);
+    return new AmqpService(options).ready();
   }
 
   constructor(options: any) {
-    this.setOptions(options);
+    this.options = options;
   }
 
-  private setOptions(options: any) {
-    this.options = {
-      hostname: options.amqp_hostname,
-      username: options.amqp_username,
-      password: options.amqp_password,
-    };
-    if (options.hasOwnProperty('protocol')) {
-      Reflect.set(this.options, 'protocol', options.amqp_protocol);
-    }
-    if (options.hasOwnProperty('port')) {
-      Reflect.set(this.options, 'port', parseInt(options.amqp_port));
-    }
-    if (options.hasOwnProperty('heartbeat')) {
-      Reflect.set(this.options, 'heartbeat', parseInt(options.amqp_heartbeat));
-    }
-    if (options.hasOwnProperty('vhost')) {
-      Reflect.set(this.options, 'vhost', options.amqp_vhost);
-    }
-  }
-
-  ready() {
+  ready(): Promise<Channel> {
     return new Promise((resolve, reject) => {
       connect(this.options, (error, conn) => {
         if (error) {
