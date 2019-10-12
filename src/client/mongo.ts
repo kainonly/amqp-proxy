ba4377505d67bf08deabef2cb39d07448005d902
kain <zhangtqx@vip.qq.com>
2019-10-12 10:38:46 +0800

    Reset

--- src/client/mongo.ts
+++
@@ -1,8 +0,0 @@
-import { MongoClient } from 'mongodb';
-
-const Mongo = (uri: string): Promise<MongoClient> => new Promise((resove, reject) => {
-  const client = new MongoClient(uri, { useNewUrlParser: true });
-  client.connect((err: any) => err ? reject(err) : resove(client));
-});
-
-export { Mongo };
