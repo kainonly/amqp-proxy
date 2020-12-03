fc42326febb25e198a944aafc380a566c102b288
kain <kainpro@foxmail.com>
2020-12-03 19:02:17 +0800

    fix transfer none

--- application/service/transfer/transfer.go
+++ application/service/transfer/transfer.go
@@ -8,20 +8,29 @@ import (
 )
 
 type Transfer struct {
+	on     bool
 	client pb.APIClient
 }
 
 func New(listen string) (c *Transfer, err error) {
 	c = new(Transfer)
+	if listen == "" {
+		c.on = false
+		return
+	}
 	var conn *grpc.ClientConn
 	if conn, err = grpc.Dial(listen, grpc.WithInsecure()); err != nil {
 		return
 	}
+	c.on = true
 	c.client = pb.NewAPIClient(conn)
 	return
 }
 
 func (c *Transfer) Push(pipe string, value interface{}) (err error) {
+	if !c.on {
+		return
+	}
 	var data []byte
 	if data, err = jsoniter.Marshal(value); err != nil {
 		return
